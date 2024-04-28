422ab8264c341d9af1989adeb9964e214803a584
Pablo Bronee <165335785+PabloBronee@users.noreply.github.com>
2024-04-28 20:09:13 -0300

    Create Bloque2_Tarea2.4_Ejercicio4.py

---
+++ Bloque2_Tarea2.4_Ejercicio4.py
@@ -0,0 +1,38 @@
+numDado = int(input("Ingresa tu numero: "))
+
+if (numDado%2 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 2?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 2?: No")
+
+if (numDado%3 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 3?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 3?: No")
+    
+if (numDado%5 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 5?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 5?: No")
+
+if (numDado%7 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 7?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 7?: No")
+
+if (numDado%9 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 9?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 9?: No")
+
+if (numDado%10 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 10?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 10?: No")
+
+if (numDado%11 == 0):
+    print("¿Es " + str(numDado) + " multiplo de 11?: Si")
+else:
+     print("¿Es " + str(numDado) + " multiplo de 11?: No")
+    
+    
