b979d0ed7dc91d2651e3f187389b043e707bbf64
Pablo Bronee <165335785+PabloBronee@users.noreply.github.com>
2024-04-28 20:05:38 -0300

    Create Bloque2_Tarea2.4_Ejercicio3.py

---
+++ Bloque2_Tarea2.4_Ejercicio3.py
@@ -0,0 +1,5 @@
+Celsius = float(input("Ingresa la temperatura en grados celsius: "))
+Fahrenheit = Celsius * 1.8 + 32
+Kelvin = Celsius + 273.15
+
+print("\nTemperatura en Celsius: " + str(Celsius) + "\nLa temperatura en Fahrenheit: " + str(Fahrenheit) + "\nLa temperatura en Kelvin: " + str(Kelvin) )
