44334d8704afde0d0342ccbe493e521a7b66c551
Pablo Bronee <165335785+PabloBronee@users.noreply.github.com>
2024-05-16 06:22:53 -0300

    Create Bloque2_Tarea2.5_Desafío2.py

---
+++ Bloque2_Tarea2.5_Desafío2.py
@@ -0,0 +1,6 @@
+PrimerNumero = float(input("Ingresa el primer numero "))
+SegundoNumero = float(input("Ingresa el segundo numero "))
+
+print(f"\n{PrimerNumero} mas {SegundoNumero}, es igual a: ", PrimerNumero + SegundoNumero,f"\n{PrimerNumero} menos {SegundoNumero}, es igual a: ", PrimerNumero - SegundoNumero,f"\n{PrimerNumero} multiplicado por {SegundoNumero}, es igual a: ", PrimerNumero * SegundoNumero,f"\n{PrimerNumero} dividido {SegundoNumero}, es igual a: ", PrimerNumero // SegundoNumero)
+
+
