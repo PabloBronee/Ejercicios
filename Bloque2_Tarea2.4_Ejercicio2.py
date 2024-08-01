243612ee6d5f85a1ba7caa2e7e565129cbbffa95
Pablo Bronee <165335785+PabloBronee@users.noreply.github.com>
2024-08-01 03:51:48 -0300

    Add files via upload

---
+++ Bloque2_Tarea2.4_Ejercicio2.py
@@ -0,0 +1,12 @@
+#Primero se determinan las cinco variables, que actuaran como las cinco calificaciones
+a = 2
+b = 3
+c = 4
+d = 5
+e = 6
+
+#Ahora se calcula el promedio, al sumar las cinco calificaciones para entonces dividirlas entre cinco
+promedio = a + b + c + d + e / 5
+
+#Se muestr en consola el promedio calculado previamente
+print(promedio)
